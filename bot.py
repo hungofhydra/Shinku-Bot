1834eb2eaf779864558ee8609743f12633d14736
Nguyễn Tấn Hùng <hungofhydraforwork@gmail.com>
2024-02-20 22:59:17 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from discord import app_commands
 class Bot(commands.Bot):
 	def __init__(self):
 		intents = discord.Intents.all()
-		self.token = "your bot token"
+		self.token = "MTIwOTUyNDU1MDY4NTk1ODE1NQ.GePm7L.J3fL-oXiGDi6VdPSbmm4N840FpZZ3ppy1-8TPY"
 		self.guildIdForOwnerCommand = #guild id you want
 		
 		
