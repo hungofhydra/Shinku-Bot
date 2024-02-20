0b0ea2c58d4032c2df9635889756a97ffdb8c538
Nguyễn Tấn Hùng <hungofhydraforwork@gmail.com>
2024-02-20 23:00:17 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,15 @@
 import discord
+import os
 from discord.ext import commands
 from discord import app_commands
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Bot(commands.Bot):
 	def __init__(self):
 		intents = discord.Intents.all()
-		self.token = "MTIwOTUyNDU1MDY4NTk1ODE1NQ.GePm7L.J3fL-oXiGDi6VdPSbmm4N840FpZZ3ppy1-8TPY"
+		self.token = os.getenv('BOT_TOKEN')
 		self.guildIdForOwnerCommand = #guild id you want
 		
 		
